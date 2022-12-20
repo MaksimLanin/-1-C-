519c86155dbca6d5eb01f95b18125af372e8f5b9
Maksim Lanin <maksimlanin@mail.ru>
2022-12-20 13:34:26 +0300

    дз1 ланинмс

---
+++ Дз_1C#(MinMax)/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите а=" );
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите b=" );
+int b = Convert.ToInt32(Console.ReadLine());
+if(a!=b){
+if(a>b)
+{
+    Console.WriteLine("max= "+a);
+    Console.WriteLine("min="+b);
+}
+ else
+{
+    Console.WriteLine("max= "+b);
+    Console.WriteLine("min= "+a);
+}
+}
+else{
+   Console.WriteLine("a и b равны, введите разные по значению числа"); 
+}
