3180ff7733929a3fab458ea82227302310bfac87
Maksim Lanin <maksimlanin@mail.ru>
2022-12-20 13:35:47 +0300

    дз4 ланинмс

---
+++ Dz_4C#(MassivChetnost)/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите размер массива = ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] myArray =  Enumerable.Range(1,size).ToArray();
+
+for(int i =0; i < myArray.Length;i++){
+if(myArray[i]%2==0){
+    Console.WriteLine(myArray[i]);
+}
+}
+
+
+
+ 
