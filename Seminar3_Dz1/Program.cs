2b77bf33d98dc848a7073754cbd4440a81cc612b
Maksim Lanin <maksimlanin@mail.ru>
2023-01-03 13:40:24 +0300

    Семинар 3 Дз1 ЛанинМС

---
+++ Seminar3_Dz1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите пятизначное число");
+string number = Console.ReadLine();
+int length = number.Length;
+if(length==5){
+    if(number[0]==number[4] && number[1]==number[3]){
+        Console.WriteLine(number +" "+ "Полиндром");
+    }
+    else
+    Console.WriteLine("Число не является Полиндромом");
+}
+else
+Console.WriteLine("Число не пятизначное");
