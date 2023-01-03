a075ca74d7532d4ccffa90632b67e61cd9f20224
Maksim Lanin <maksimlanin@mail.ru>
2023-01-03 15:53:55 +0300

    Семинар3 Дз3 ЛанинМС

---
+++ Seminar3_Dz3/Program.cs
@@ -0,0 +1,12 @@
+﻿int x = ReadInt("Введите число: ");
+for(int i=1; i<=x ;i++){
+
+    Console.Write($"{i*i*i}, ");
+}
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
