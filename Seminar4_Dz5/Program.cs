8cad0b382f354ab4d8e87dcfe56f89faacfa944d
Maksim Lanin <maksimlanin@mail.ru>
2023-01-13 11:46:01 +0300

    Семинар 4 дз5 ланинмс

---
+++ Seminar4_Dz5/Program.cs
@@ -0,0 +1,19 @@
+﻿int numberA = ReadInt("Введите число A: ");
+            int numberB = ReadInt("Введите число B: ");
+            ToDegree(numberA, numberB);
+            
+            void ToDegree(int a, int b)
+            {
+                int result = 1;
+                for (int i = 1; i <= b; i++)
+                {
+                    result = result * a;
+                }
+                Console.WriteLine($"Возведение числа {a} в степень {b} = {result}");
+            }
+
+            int ReadInt(string message)
+            {
+                Console.Write( message);
+                return Convert.ToInt32(Console.ReadLine());
+            }
