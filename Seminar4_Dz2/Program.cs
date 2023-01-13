fcead042e126f8ca9c2a831f195980f3781b2260
Maksim Lanin <maksimlanin@mail.ru>
2023-01-13 09:15:13 +0300

    Семинар4 дз2 ЛанинМС

---
+++ Seminar4_Dz2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число А: ");
+int A = Convert.ToInt32(Console.ReadLine());
+int count =0;
+while(A>0){
+    A/=10;
+    count++;
+}
+Console.WriteLine($"Цифр в числе: {count}");
