0f57c4aaf99ded2156af62f86d332b8ab3629e1c
Maksim Lanin <maksimlanin@mail.ru>
2023-01-13 09:58:40 +0300

    Семинар4 дз4 ЛанинМС

---
+++ Seminar4_Dz4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите размер массивa: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[n];
+Random rand = new Random();
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = rand.Next(0,2);
+}
+Console.WriteLine(string.Join(", ", arr));
