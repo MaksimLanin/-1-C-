d95b3a9c5f95b489487ec164a79d95c4d1c653ef
Maksim Lanin <maksimlanin@mail.ru>
2023-01-13 09:44:50 +0300

    Семинар4 дз3 ЛанинМС

---
+++ Seminar4_Dz3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+int res=1;
+for(int i = 1; i <= A; i++)
+{
+    res=res* i;
+}
+Console.WriteLine("Ответ: "+res);
+
+
