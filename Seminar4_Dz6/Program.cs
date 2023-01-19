505f7d647ba6678587f433e8295beefe50a8a057
Maksim Lanin <maksimlanin@mail.ru>
2023-01-19 15:10:50 +0300

    Семинар4 Дз6 ЛанинМС

---
+++ Seminar4_Dz6/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.Write("Введите любое число (A): ");
+int enterNumber = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[enterNumber];
+
+for (int i = 0; i < enterNumber; i++)
+{
+    array[i] = new Random().Next(enterNumber+1);
+    Console.Write(array[i] + ", ");
+}
+Console.WriteLine();
