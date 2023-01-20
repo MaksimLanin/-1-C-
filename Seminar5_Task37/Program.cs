ddab2a845391e0f55ce5b2ea9155cc228961d4d0
Maksim Lanin <maksimlanin@mail.ru>
2023-01-20 19:36:59 +0300

    Семинар5 задание37 ланинмс

---
+++ Seminar5_Task37/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Задайте массив натуральных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+*/
+
+
+int [] GetRandomArray(int size, int LeftRange, int RightRange)
+{
+int[] array = new int [size];
+for(int i=0; i < size;i++){
+    array[i]=new Random().Next(LeftRange,RightRange+1);
+}
+return array;
+}
+int[] massive=GetRandomArray(10,0,100);
+Console.WriteLine(string.Join(", ",massive));
+
+int sum=0;
+int min=int.MaxValue;
+int max=int.MinValue;
+ for(int i = 0; i < massive.Length; i++){
+    if(massive[i]>max){
+        max=massive[i];
+    }
+    if (massive[i] < min)
+    {
+        min=massive[i];
+    }
+    sum=max-min;
+     
+ }
+Console.WriteLine(@$"Максимальное число: {max}
+ Минимальное число: {min} 
+ Разность: {sum}");
