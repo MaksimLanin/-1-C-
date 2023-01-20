3611ecf3c3e4dce7d3d35cec24ce238d4b33b4fe
Maksim Lanin <maksimlanin@mail.ru>
2023-01-20 19:16:59 +0300

    семинар5 задание 35 ланинмс

---
+++ Seminar5_Task35/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Задайте одномерный массив из 123 случайных чисел в диапазоне [0, 150]. 
+Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+[5, 18, 123, 6, 2] -> 1
+[1, 2, 3, 6, 2] -> 0
+[10, 11, 12, 13, 14] -> 5
+
+*/
+
+int [] GetRandomArray(int size, int LeftRange, int RightRange)
+{
+int[] array = new int [size];
+for(int i=0; i < size;i++){
+    array[i]=new Random().Next(LeftRange,RightRange+1);
+}
+return array;
+}
+int[] massive=GetRandomArray(123,0,150);
+
+
+int count = 0;
+
+for (int i = 0; i < massive.Length; i++)
+{
+if (i>=10 && i<=99)
+count++;
+}
+Console.WriteLine($"В массиве из {massive.Length} чисел, в диапазоне от 10 до 99 включительно находится {count} чисел");
