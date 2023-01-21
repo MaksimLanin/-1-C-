77e7bbe122924d646bb9d64abf418e4f18ed8aef
Maksim Lanin <maksimlanin@mail.ru>
2023-01-21 11:20:29 +0300

    Семинар5 задание 34 ЛанинМС

---
+++ Seminar5_Task34/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2
+
+*/
+int [] GetRandomArray(int size, int LeftRange, int RightRange)
+{
+int[] array = new int [size];
+for(int i=0; i < size;i++){
+    array[i]=new Random().Next(LeftRange,RightRange+1);
+}
+return array;
+}
+int[] massive=GetRandomArray(4,99,999);
+Console.WriteLine(string.Join(", ",massive));
+
+int count = 0;
+
+for (int i = 0; i < massive.Length; i++)
+{
+if (massive[i] % 2 == 0)
+count++;
+}
+Console.WriteLine($"из {massive.Length} чисел, {count} четных");
