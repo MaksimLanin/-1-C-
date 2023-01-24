1e510039067085e43dcf98308b3e2f36a27edca4
Maksim Lanin <maksimlanin@mail.ru>
2023-01-24 17:57:47 +0300

    Семинар7 задание50 ланинмс

---
+++ Seminar7_Task50/Program.cs
@@ -0,0 +1,48 @@
+﻿Console.Write("Введите размер строки: ");
+int rowsLenght = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите размер столбца: ");
+int columsLenght = Convert.ToInt32(Console.ReadLine());
+
+int [,] matrix = new int [rowsLenght,columsLenght];
+
+getRandomMatrix(matrix);
+printMatrix(matrix);
+
+Console.Write("Введите номер строки: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите номер столбца: ");
+int colums = Convert.ToInt32(Console.ReadLine());
+
+if (rows > matrix.GetLength(0) || colums > matrix.GetLength(1))
+{
+    Console.WriteLine("Введите другое значение, такого элемента нет");
+}
+else
+{
+    Console.WriteLine($" {matrix[rows-1,colums-1]}");
+}
+
+void getRandomMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {        
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix [i,j] = new Random().Next(0,100);
+        }   
+    }
+}
+
+void printMatrix(int[,] matrix){
+    for(int i = 0; i< matrix.GetLength(0);i++)
+    {
+        for(int j = 0; j<matrix.GetLength(1);j++)
+        {
+           Console.Write(matrix[i,j] + " ");
+        }
+        
+        Console.WriteLine();
+    }
+}
+
+
