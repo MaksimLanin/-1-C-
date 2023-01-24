64b415dd6dce4072aa3fb81c107582c3ca8cb010
Maksim Lanin <maksimlanin@mail.ru>
2023-01-24 17:58:50 +0300

    Семинар7 задание47 ланинмс

---
+++ Seminar7_Task47/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами.
+double[,] getRandomMatrix(int rows, int colums, int leftRange, int rightRange)
+{
+    double[,] matrix = new double[rows,colums];
+    for(int i = 0; i< matrix.GetLength(0);i++)
+    {
+        for(int j = 0; j<matrix.GetLength(1);j++)
+        {
+            matrix[i,j] = Convert.ToDouble(new Random().Next(leftRange, rightRange+1))/10;
+            
+        }
+    }
+    return matrix;
+}
+void printMatrix(double[,] matrix){
+    for(int i = 0; i< matrix.GetLength(0);i++)
+    {
+        for(int j = 0; j<matrix.GetLength(1);j++)
+        {
+           Console.Write(matrix[i,j] + " ");
+        }
+        
+        Console.WriteLine();
+    }
+}
+
+
+double[,] array = getRandomMatrix(3,4,0,10);
+printMatrix(array);
