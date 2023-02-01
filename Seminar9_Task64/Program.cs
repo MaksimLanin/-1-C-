275b57728aa931fa67417a122cbd417864f246a0
Maksim Lanin <maksimlanin@mail.ru>
2023-02-01 13:53:43 +0300

    Семинар9 задание64 ЛанинМС

---
+++ Seminar9_Task64/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+FromLessToMore(number,count);
+
+void FromLessToMore(int n, int count)
+{
+    if (count > n)
+    {
+        return;
+    }
+    else
+    {
+        FromLessToMore(n, count + 1);
+        Console.Write(count + " ");
+    }
+}
